b00fa88d8fcb2cb6549b57796ab37b715aab0a4e
hassamarif <arifhassam18@gmail.com>
2023-03-17 00:29:07 +0500

    updates pushed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
     <div className="App">
       <h1>this is a media player</h1>
+      <ReactPlayer url={vidUrl} playing={false} volume={0.5} />
     </div>
   );
 }
