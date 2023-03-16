658fa3e63c7ac820dddd9bb56b3b9dbe880dc0e0
hassamarif <arifhassam18@gmail.com>
2023-03-17 02:14:44 +0500

    updates pushed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import ReactPlayer from "react-player";
+import { useState } from "react";
 
 function App() {
+  // const [vidUrl, setvidUrl] = useState("");
+  const [url, setUrl] = useState("");
+
+  // const handleClick = () => {
+  //   setUrl(url);
+  // };
   return (
     <div className="App">
-      <h1>this is a media player</h1>
-      <ReactPlayer url={vidUrl} playing={false} volume={0.5} />
+      <h1>React Video Player</h1>
+
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Your Link"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          {/* <button onClick={handleClick}>Play</button> */}
+        </div>
+      </div>
+
+      <ReactPlayer url={url} playing={false} volume={0.5} />
     </div>
   );
 }
